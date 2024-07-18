7f0510dcbbccc13ff1aa2109740400439a216ff7
VicenzoMF <vicenzomf@gmail.com>
2024-07-18 10:18:19 -0300

    editing features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
+    <div className="flex gap-4 my-32">
       <Card type='add' />
-      <Card type='task' />
+      <Card type='task' content="During Tania’s party last week someone broke the sofa" date="08/08/2024" title="Urien mano" />
+      <Card type='task' content="During Tania’s party last week someone broke the sofa" title="Urien"/>
+
+    </div>
 
     </>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,63 +1,79 @@
-import Image from "next/image"
-import add_plus from "@/assets/ic_baseline-plus.png"
+'use client'
+import Image from "next/image";
+import { Checkmark } from "@/assets/checkmark";
+import { Plus } from "@/assets/plus";
+import { Pencil } from "@/assets/pencil";
+import { Trash } from "@/assets/trash";
+import { useState } from "react";
 
 interface CardProps {
-    type: string
-    categories?: string
+  type: string;
+  categories?: string;
+  content?: string;
+  date?: string;
+  title?: string;
 }
-export function Card({ type = 'add', categories }: CardProps) {
+export function Card({ type = "add", categories, content, date, title }: CardProps) {
+  const [editing, setEditing] = useState(false);
+  
 
-    return (
-        <div className="w-[15.625rem] h-[15.625rem] rounded-3xl shadow-lg">
-            {type == 'add' ? (
-                <><div className="h-[28%] bg-[#367C93] rounded-t-3xl flex items-center justify-center">
-                    <h3 className="font-extrabold text-lg">Add task</h3>
-                </div><div className="h-2/3 flex items-center justify-center">
-                        <Image src={add_plus}
-                            width={150}
-                            height={150}
-                            alt="Picture of the author" />
-                    </div></>
-            ) : type == "task" ? (
-                <><div className="h-[28%] bg-[#367C93] rounded-t-3xl flex items-center justify-center">
-                    <h3 className="font-extrabold text-lg">Add task</h3>
-                </div>
-                    <div className="px-3">
-                        <div>
-                            <Image src={add_plus}
-                                width={50}
-                                height={50}
-                                alt="Picture of the author" />
-                        </div>
-                        <div>
-                            <p className="text-black/[.25] text-sm">No deadline</p>
-                        </div>
-                        <div>
-                            <p className="text-sm">During Tania’s party last week someone broke the sofa</p>
-                        </div>
-                        <div className="flex flex-row justify-between">
-                            <div>
-                                <p className="text-sm text-black/[.25]">Added by Arthur</p>
-                            </div>
-                            <div className="flex flex-row">
-                                <Image src={add_plus}
-                                    width={25}
-                                    height={25}
-                                    alt="Picture of the author" />
-                                <Image src={add_plus}
-                                    width={25}
-                                    height={25}
-                                    alt="Picture of the author" />
-                                <Image src={add_plus}
-                                    width={25}
-                                    height={25}
-                                    alt="Picture of the author" />
-                            </div>
-                        </div>
-                    </div>
-                </>
-            ) : null
+  const handleToggleEditing = () => {
+    setEditing(!editing)
+  }
+  return (
+    <div className="w-[15.625rem] h-[15.625rem] rounded-3xl shadow-lg">
+      {type == "add" ? (
+        <>
+          <div className="h-[28%] bg-[#367C93] rounded-t-3xl flex items-center justify-center">
+            <h3 className="font-extrabold text-lg">Add task</h3>
+          </div>
+          <Plus className="mx-auto cursor-pointer" opacity={.25} width={150} height={150}/>
+          <div className="h-2/3 flex items-center justify-center "></div>
+        </>
+      ) : type == "task" ? (
+        <>
+          <div className="h-[28%] bg-[#367C93] rounded-t-3xl flex items-center justify-center">
+            { editing ? (
+              <textarea className="h-6 bg-transparent resize-none outline-none text-center font-extrabold"/>
+            ) : (
+              <h3 className="font-extrabold text-lg">{title}</h3>
+              )
             }
-        </div >
-    )
+            </div>
+          <div className="flex flex-col mx-auto items-center  -translate-y-4">
+            <div className="w-[35px] bg-white rounded-full">
+            <Plus width={35} height={35}/>
+            </div>
+            { editing ? (
+                <input type="date" className="text-black/[.25] text-sm text-center"/>
+            ) : (
+              <p className="text-sm text-black/[.25]">{date == null ? "No deadline" : "Due " + date }</p>
+            )
+            }
+          </div>
+          <div className="flex flex-col px-3 justify-between h-[43%]">
+            { editing ? (
+              <div>
+                <textarea className="text-sm w-full h-full resize-none" >{content}</textarea>
+              </div>) : (
+              <div>
+                <p className="text-sm">{content}</p>
+              </div>
+            )}
+            <div className="flex flex-row justify-between">
+              <div>
+                <p className="text-sm text-black/[.25]">Added by Arthur</p>
+              </div>
+              <div className="flex flex-row">
+                <Checkmark className="cursor-pointer" width={25} height={25}/>
+                <Pencil className="cursor-pointer" onClick={handleToggleEditing} opacity={.50} width={25} height={25} />
+                <Trash className="cursor-pointer" opacity={.50} width={25} height={25}/>
+              </div>
+            </div>
+          </div>
+
+        </>
+      ) : null}
+    </div>
+  );
 }
